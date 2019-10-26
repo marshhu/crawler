20381716c87a21e2f3de21d1f60e3cbd99abd83a
marsh <marshhu@qq.com>
2019-10-26 18:24:28 +0800

    转码utf8

--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
-	"net/http"
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"golang.org/x/net/html/charset"
 )
 
-func main(){
-	resp,err := http.Get("http://www.zhenai.com/zhenghun")
-	if err != nil{
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error status code ",resp.StatusCode)
+		fmt.Println("Error status code ", resp.StatusCode)
 		return
 	}
-
-	all,err := ioutil.ReadAll(resp.Body)
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n",all)
+	fmt.Printf("%s\n", all)
+}
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes,"")
+	return e
 }
