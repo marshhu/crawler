f2df0c5ada282e0ba4022da30754df22b8f6d77d
marshhu <marshhu@qq.com>
2019-10-26 21:00:48 +0800

    正则获取城市信息

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/transform"
 	"golang.org/x/net/html/charset"
+	"regexp"
 )
 
 func main() {
@@ -29,7 +30,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", all)
+	// fmt.Printf("%s\n",all)
+	printCityList(all)
 }
 
 func determineEncoding(r io.Reader) encoding.Encoding {
@@ -39,4 +41,17 @@ func determineEncoding(r io.Reader) encoding.Encoding {
 	}
 	e, _, _ := charset.DetermineEncoding(bytes,"")
 	return e
+}
+
+func printCityList(contents []byte){
+	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	match := re.FindAllSubmatch(contents,-1)
+	for _,m := range match{
+	// 	for _,subMatch := range m{
+	// 		fmt.Printf("%s ",subMatch)
+	// 	}
+    //    fmt.Println()
+	fmt.Printf("City: %s, URL: %s\n",m[2],m[1])
+	}
+	fmt.Printf("Matches found: %d\n",len(match))
 }
